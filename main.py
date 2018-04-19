3e0cc6f1f3bd6192e68b1db1ee410e91ac3e3f5a
Samuel Tekle <tsamtek@yahoo.com>
2018-04-19 18:03:58 -0500

    Final Version

--- main.py
+++ main.py
@@ -24,12 +24,26 @@ def newpost():
     if request.method == 'POST':
         title = request.form['blog']
         body = request.form['body']
-        new_blog = Blog(title,body)
-        db.session.add(new_blog)
-        db.session.commit()
-       
+        title_error = ''
+        body_error = ''
+        if len(title) == 0:
+            title_error = 'Invalid Title'
+            return render_template('new_post.html',title_error=title_error)
+        elif len(body) == 0:
+            body_error = 'Blog body can not be empty'
+            return render_template('new_post.html',body_error=body_error)
+        else:
+            if not title_error and not body_error:
+                new_blog = Blog(title,body)
+                db.session.add(new_blog)
+                db.session.commit()
+                return render_template('ind_blog.html',title="Build a Blog!", 
+                blog=new_blog)
+            else: 
+                return render_template('new_post.html')
+            
     return render_template('new_post.html')
-       
+  
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
